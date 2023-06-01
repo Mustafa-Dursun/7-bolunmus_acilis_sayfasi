98eff1c6835718fbda8f0d5982937c08196b60f1
Mustafa Dursun <dursunmustafainfo@gmail.com>
2023-06-01 20:05:36 +0300

    ekleme

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const div1 = document.querySelector('.div1');
+const div1Baslik = document.querySelector('.div1Baslik');
+const div2Baslik = document.querySelector('.div2Baslik');
 const div2 = document.querySelector('.div2');
 const container = document.querySelector('.container');
+
 
 
 div1.addEventListener('mouseover', () =>{
     container.classList.remove('showBack');
     container.classList.add('showFront');
+    div2Baslik.classList.remove('buyuk');
+    div1Baslik.classList.add('buyuk');
 })
 
 div2.addEventListener('mouseover', () =>{
     container.classList.remove('showFront');
     container.classList.add('showBack');
+    div1Baslik.classList.remove('buyuk');
+    div2Baslik.classList.add('buyuk');
 })
+
 
 container.addEventListener('mouseout', () =>{
     container.setAttribute('class','container');
+    div1Baslik.classList.remove('buyuk');
+    div2Baslik.classList.remove('buyuk');
 })
+
+
